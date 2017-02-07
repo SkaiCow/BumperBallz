08e38a3c599f6dd9aa6921ab8b2511c8cdc6aea4
William Lasater <WillALasater@gmail.com>
2017-02-06 22:05:32 -0600

    First

---
+++ Testing/Assets/Scripts/FallowCamera.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FallowCamera : MonoBehaviour {
+	
+	public float minScale;
+	private int numOfPlayers;
+	private bool acounted1;
+	private bool acounted2;
+	private bool acounted3;
+	private bool acounted4;
+	private float X;
+	private float Y;
+	public float camraSpeed;
+	private float setX;
+	private float setY;
+
+	// Use this for initialization
+	void Start () {
+		
+		numOfPlayers = 4;
+	
+	}
+	
+	
+	void Update()
+	{
+		
+		if(GameObject.Find("Player1") == null & !acounted1)
+		{
+			numOfPlayers--;
+			acounted1 = true;
+		}
+		if(GameObject.Find("Player2") == null & !acounted2)
+		{
+			numOfPlayers--;
+			acounted2 = true;
+		}
+		if(GameObject.Find("Player3") == null & !acounted3)
+		{
+			numOfPlayers--;
+			acounted3 = true;
+		}
+		if(GameObject.Find("Player4") == null & !acounted4)
+		{
+			numOfPlayers--;
+			acounted4 = true;
+		}
+		
+		//print(numOfPlayers);
+		
+	}
+	
+	void FixedUpdate ()
+	{
+		
+		if(!acounted1)
+		{
+			X = GameObject.Find("Player1").transform.position.x;
+			Y = GameObject.Find("Player1").transform.position.y;
+		}
+		
+		if(!acounted2)
+		{
+			
+			X = (X + GameObject.Find("Player2").transform.position.x) / 2;
+			Y = (Y + GameObject.Find("Player2").transform.position.y) / 2;
+			
+		}
+		if(!acounted3)
+		{
+			
+			X = (X + GameObject.Find("Player3").transform.position.x) / 2;
+			Y = (Y + GameObject.Find("Player3").transform.position.y) / 2;
+			
+		}
+		if(!acounted4)
+		{
+			
+			X = (X + GameObject.Find("Player4").transform.position.x) / 2;
+			Y = (Y + GameObject.Find("Player4").transform.position.y) / 2;
+			
+		}
+		
+		
+		transform.position = new Vector3(X , Y , -47.704f);
+		//print(transform.position);
+	
+	}
+}
---
+++ Testing/Assets/Scripts/Player_control.cs
@@ -0,0 +1,113 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_control : MonoBehaviour {
+	
+		// Use this for initialization
+		
+	
+	public int player_Num;
+	public Rigidbody2D rig;
+	public float punchPower;
+	public float superPunch;
+	public float boost_delay;
+	private float waitFor;
+	private bool playerFell;
+	public float scaleDownDelay;
+	public Light light;
+	private float maxRange;
+	
+	
+	
+	void Start () {
+		
+		waitFor = 0;
+		rig = GetComponent<Rigidbody2D>();
+		scaleDownDelay = Time.time + scaleDownDelay;
+		light = light.GetComponent<Light>();
+		maxRange = light.range;
+		
+	}
+		
+	void Update() {
+		if(!playerFell)
+		{
+			bool hor = Input.GetAxis("J" + player_Num + "_Hor") > .1 || Input.GetAxis("J" + player_Num + "_Hor") < -.1;
+			bool vert = Input.GetAxis("J" + player_Num + "_Ver") > .1 || Input.GetAxis("J" + player_Num + "_Ver") < -.1;
+			Move_Player(hor,vert);
+			
+			//print(Input.GetAxis("J" + player_Num + "_Hor"));
+			
+			bool boost = Input.GetButton("J" + player_Num + "_Boost");
+			Boost_Player(boost);
+			
+			if(light.range < maxRange)
+			{
+				light.range += .1f;
+			}
+		}
+	}
+		
+	void FixedUpdate () {
+		if(playerFell)
+		{
+			playerFelling();
+		}
+	}
+	
+	void OnTriggerExit2D(Collider2D other)
+	{
+		
+		playerFell = true;
+		//print("fell");
+		
+	}
+	
+	void Move_Player (bool X, bool Y)
+	{
+		
+		if(X)
+		{
+			rig.AddForce(transform.right * punchPower * Input.GetAxis("J" + player_Num + "_Hor") * Time.deltaTime);
+		}
+		
+		if(Y)
+		{
+			rig.AddForce(transform.up * punchPower * Input.GetAxis("J" + player_Num + "_Ver") * Time.deltaTime);
+		}
+		
+	}
+	
+	void Boost_Player (bool boost){
+		
+		
+		
+		if(boost && Time.time > waitFor)
+		{
+			
+			rig.AddForce(transform.right * superPunch * Input.GetAxis("J" + player_Num + "_Hor") * Time.deltaTime);
+			rig.AddForce(transform.up * superPunch * Input.GetAxis("J" + player_Num + "_Ver") * Time.deltaTime);
+			light.range = maxRange - 5;
+			print("BOOST");
+			waitFor = Time.time + boost_delay;
+		}
+		
+	}
+	
+	void playerFelling()
+	{
+		
+		if(transform.localScale.x >= 0)
+		{
+			transform.localScale += new Vector3(-.05F, -.05F, 0);
+			scaleDownDelay = Time.time + scaleDownDelay;
+		}else
+		{
+			Destroy (gameObject);
+		}
+		//print("felling");
+		
+	}
+	
+	
+}
